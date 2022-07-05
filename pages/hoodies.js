0178534f8665d83fa7bb3b84bec14580c3759e80
PARTH GOHIL <parth.gohil19@gmail.com>
2022-07-05 20:30:04 +0530

    {Object.keys(products).length === 0 && <p>Sorry all the hoodies are currently out of stock. New stock coming soon. Stay Tuned!</p> }

--- pages/hoodies.js
+++ pages/hoodies.js
@@ -31,7 +31,6 @@ const Hoodies = ({ products }) => {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 justify-center">
-            {Object.keys(products).length === 0 && <p>Sorry all the hoodies are currently out of stock. New stock coming soon. Stay Tuned!</p> }
             {Object.keys(products).map((item) => {
               return (
                   <Link passHref={true} key={products[item]._id} href={`/product/${products[item].slug}`}>
